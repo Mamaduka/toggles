6385d7bc84ae4ebfcc3832ffded570575bb6e88e
George Mamadashvili <georgemamadashvili@gmail.com>
2021-02-04 23:32:53 +0400

    Fixes open state toggling issue

--- src/edit.js
+++ src/edit.js
@@ -7,27 +7,44 @@ import {
 	useBlockProps,
 	RichText,
 } from '@wordpress/block-editor';
+import { useSelect } from '@wordpress/data';
 import { SPACE } from '@wordpress/keycodes';
 
-export default function TogglesEdit( { attributes, setAttributes } ) {
+export default function TogglesEdit( {
+	attributes,
+	clientId,
+	isSelected,
+	setAttributes,
+} ) {
+	const isInnerBlockSelected = useSelect(
+		( select ) =>
+			select( 'core/block-editor' ).hasSelectedInnerBlock( clientId ),
+		[ clientId ]
+	);
+
 	const blockProps = useBlockProps( {
 		className: 'wp-block-toggles',
+		open: isSelected || isInnerBlockSelected,
 	} );
 
-	// This is called onKeyUp on <details> and cancel default behavior.
-	function cancelSpaceToggle( event ) {
+	// Don't toggle open value on space.
+	function keyUpListener( event ) {
 		if ( event.keyCode === SPACE ) {
 			event.preventDefault();
 		}
+	}
+
+	// Don't toggle open value on click.
+	function clickListeer( event ) {
+		event.preventDefault();
 	}
 
 	return (
 		<>
 			<BlockControls />
 
-			{ /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */ }
-			<details { ...blockProps } onKeyUp={ cancelSpaceToggle }>
-				<summary>
+			<details { ...blockProps }>
+				<summary onKeyUp={ keyUpListener } onClick={ clickListeer }>
 					<RichText
 						tagName="span"
 						value={ attributes.summary }
